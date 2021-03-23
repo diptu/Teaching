89d034d6cb97b7d3bd6c685a02d4e390e0aac191
diptu <diptunazmulalam@gmail.com>
2021-03-24 01:54:56 +0600

    225L : Lab 04

---
+++ CSE 225L Data Structures and Algorithms/Resources/Lab codes/Lab 04/studentInfo.h
@@ -0,0 +1,25 @@
+#ifndef STUDENTINFO_H_INCLUDED
+#define STUDENTINFO_H_INCLUDED
+#include<iostream>
+using namespace std;
+class StudentInfo
+{
+    friend bool operator!=(StudentInfo &a,StudentInfo &b);
+    friend bool operator==(StudentInfo &a,StudentInfo &b);
+private:
+    int id;
+    string name;
+    float cgpa;
+public:
+    StudentInfo();
+    StudentInfo(int, string, float);
+    void setID(int);
+    void setName(string);
+    void setCGPA(float);
+    int getID();
+    string getName();
+    float getCGPA();
+    void Print();
+};
+
+#endif // STUDENTINFO_H_INCLUDED
